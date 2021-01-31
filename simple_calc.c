a0ab63f16e9ff79695a9464d013eb1919f9ef5d6
adinapersson <77530007+adinapersson@users.noreply.github.com>
2021-01-31 16:32:07 +0100

    Add files via upload

---
+++ simple_calc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    double a,b;
+    printf("Enter operator:");
+    scanf("%c", &op);
+    printf("Enter two numbers:");
+    scanf("%lf %lf", &a, &b);
+
+    switch (op) {
+        case '+':
+        printf("%.1lf + %.1lf = %.1lf", a, b, a+b);
+        break;
+        case '-':
+        printf("%.1lf - %.1lf = %.1lf", a, b, a-b);
+        break;
+        case '*':
+        printf("%.1lf * %.1lf = %.1lf", a, b, a*b);
+        break;
+        case '/':
+        printf("%.1lf / %.1lf = %.1lf", a, b, a/b);
+        break;
+        default:
+        printf("Error! Operator is not correct");
+    }
+    return 0;
+}
